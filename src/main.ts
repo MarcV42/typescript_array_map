a2f0b037d2b83e056d36e684f31567e3f2fb87fc
MVe11111 <90624942+Sprudelflasch@users.noreply.github.com>
2023-09-26 10:34:44 +0200

    Done

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+// Schritt 1
+const numbers = [1, 2, 3, 4, 5];
+const doubledNumbers = numbers.map((x) => x * 2);
+console.log(doubledNumbers); // Gibt [2, 4, 6, 8, 10] aus
+
+// Schritt 2
+const words = ['Apfel', 'Banane', 'Kirsche', 'Erdbeere', 'Mango'];
+const longerWords = words.filter((word) => word.length > 5);
+console.log(longerWords); // Gibt ['Banane', 'Kirsche', 'Erdbeere'] aus
+
+
+// Schritt 3
+// const numbers = [1, 2, 3, 4, 5];
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+console.log(sum); // Gibt 15 aus
+
+
+// Schritt 4
+const numbersHigh = [5, 8, 12, 3, 7];
+const isGreaterThan10 = numbersHigh.some((x) => x > 10);
+console.log(isGreaterThan10); // Gibt true aus, da mindestens eine Zahl größer als 10 ist
