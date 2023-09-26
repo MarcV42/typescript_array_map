13702834d48afe6df45d4b80193968581a826913
MVe11111 <90624942+Sprudelflasch@users.noreply.github.com>
2023-09-26 12:36:30 +0200

    let

--- src/main.ts
+++ src/main.ts
@@ -9,23 +9,23 @@ Erstellt ein neues TypeScript-Projekt in IntelliJ und führt die folgenden leich
 	*/
 
 // Schritt 1
-const numbers = [1, 2, 3, 4, 5];
-const doubledNumbers = numbers.map((x) => x * 2);
+let numbers = [1, 2, 3, 4, 5];
+let doubledNumbers = numbers.map((x) => x * 2);
 console.log(doubledNumbers); // Gibt [2, 4, 6, 8, 10] aus
 
 // Schritt 2
-const words = ['Apfel', 'Banane', 'Kirsche', 'Erdbeere', 'Mango'];
-const longerWords = words.filter((word) => word.length > 5);
+let words = ['Apfel', 'Banane', 'Kirsche', 'Erdbeere', 'Mango'];
+let longerWords = words.filter((word) => word.length > 5);
 console.log(longerWords); // Gibt ['Banane', 'Kirsche', 'Erdbeere'] aus
 
 
 // Schritt 3
 // const numbers = [1, 2, 3, 4, 5];
-const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+let sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 console.log(sum); // Gibt 15 aus
 
 
 // Schritt 4
-const numbersHigh = [5, 8, 12, 3, 7];
-const isGreaterThan10 = numbersHigh.some((x) => x > 10);
+let numbersHigh = [5, 8, 12, 3, 7];
+let isGreaterThan10 = numbersHigh.some((x) => x > 10);
 console.log(isGreaterThan10); // Gibt true aus, da mindestens eine Zahl größer als 10 ist
